278ebd2bf498cc2aa6fa938fa044f9d5a8f900e1
denis <123486371+BobColdd@users.noreply.github.com>
2026-09-17 10:43:23 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+```python
+from flask import Flask, Response, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -12,9 +13,11 @@ login_manager.login_message_category = "info"
 
 migrate = Migrate()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
     migrate.init_app(app, db)
     db.init_app(app)
     login_manager.init_app(app)
@@ -39,7 +42,50 @@ def create_app():
     app.register_blueprint(admin_bp)
     app.register_blueprint(api_bp)
 
+    # ---------------------------------------------------------
+    # ROBOTS.TXT
+    # ---------------------------------------------------------
+    @app.route("/robots.txt")
+    def robots():
+        sitemap_url = url_for("sitemap", _external=True)
+
+        robots_txt = f"""User-agent: *
+Allow: /
+
+Sitemap: {sitemap_url}
+"""
+
+        return Response(robots_txt, mimetype="text/plain")
+
+    # ---------------------------------------------------------
+    # SITEMAP.XML
+    # ---------------------------------------------------------
+    @app.route("/sitemap.xml")
+    def sitemap():
+        # Public pages that Google should discover.
+        pages = [
+            url_for("main.index", _external=True),
+        ]
+
+        sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+"""
+
+        for page in pages:
+            sitemap_xml += f"""    <url>
+        <loc>{page}</loc>
+    </url>
+"""
+
+        sitemap_xml += "</urlset>"
+
+        return Response(sitemap_xml, mimetype="application/xml")
+
+    # ---------------------------------------------------------
+    # DATABASE
+    # ---------------------------------------------------------
     with app.app_context():
         db.create_all()
 
     return app
+```
